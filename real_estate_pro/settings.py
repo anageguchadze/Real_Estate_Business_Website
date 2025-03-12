50963a47e4f409783ddf1954c071568e0ca171c3
Ana Geguchadze <anageguchadze22@gmail.com>
2025-03-12 19:38:52 +0100

    Add email

--- real_estate_pro/settings.py
+++ real_estate_pro/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,10 +147,10 @@ REST_FRAMEWORK = {
 }
 
 
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_PASSWORD = 'mzff gcbi qgaw bekk'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'anamr6211@gmail.com'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'anamr6211@gmail.com'
