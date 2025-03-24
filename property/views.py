1e818568d8867fcc59501563bf1a60fcb91f3bec
Ana Geguchadze <anageguchadze22@gmail.com>
2025-03-24 21:09:26 +0100

    Change filters

--- property/views.py
+++ property/views.py
@@ -15,12 +15,10 @@ class PropertyTypeViewSet(viewsets.ModelViewSet):
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    # filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_class = PropertyFilter
     search_fields = ["location", "property_type__name"]
-    ordering_fields = ["price", "size", "build_year"]
-    ordering = ["price"]
-    ordering = ["build_year"]
+   
 
     def get_queryset(self):
         return Property.objects.all().distinct()
@@ -34,22 +32,33 @@ class PropertyFilterOptionsView(APIView):
         # Get unique property types
         property_types = list(PropertyType.objects.values_list('name', flat=True).distinct())
 
-        # Get price ranges (modify as needed)
-        price_values = list(Property.objects.values_list('price', flat=True).distinct())
-        price_ranges = sorted(set([f"${int(price)} - ${int(price) + 50000}" for price in price_values]))
+        # Get distinct build years, ignoring NULL values
+        build_years = sorted(filter(None, Property.objects.values_list('build_year', flat=True).distinct()))
 
-        # Get property sizes
-        sizes = list(Property.objects.values_list('size', flat=True).distinct())
+        # Define price ranges manually
+        price_ranges = [
+            "$0 - $50,000",
+            "$50,000 - $100,000",
+            "$100,000 - $500,000",
+            "$500,000 - $1,000,000",
+            "$1,000,000+"
+        ]
 
-        # Get build years
-        build_years = list(Property.objects.values_list('build_year', flat=True).distinct())
+        # Define property size ranges
+        size_ranges = [
+            "0 - 50 sqm",
+            "50 - 100 sqm",
+            "100 - 200 sqm",
+            "200 - 500 sqm",
+            "500+ sqm"
+        ]
 
         return Response({
             "locations": locations,
             "property_types": property_types,
-            "pricing_ranges": price_ranges,
-            "property_sizes": sizes,
             "build_years": build_years,
+            "price_ranges": price_ranges,
+            "size_ranges": size_ranges,
         })
 
 
