eae0be9fe01f6390d7d47e446625f2d7aedb3158
Ana <anageguchadze22@gmail.com>
2025-04-07 19:02:43 +0400

    Add services in urls.py

--- real_estate_pro/settings.py
+++ real_estate_pro/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'property',
     'home',
     'about',
+    'services',
 ]
 
 MIDDLEWARE = [
--- real_estate_pro/urls.py
+++ real_estate_pro/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('property/', include('property.urls')),
     path('home/', include('home.urls')),
     path('about/', include('about.urls')),
+    path('services/', include('services.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
