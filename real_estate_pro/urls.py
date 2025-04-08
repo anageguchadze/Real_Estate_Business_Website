b10abca8530d4744d231743982931a23472fe0ca
Ana <anageguchadze22@gmail.com>
2025-04-08 19:22:18 +0400

    Add app contact

--- real_estate_pro/urls.py
+++ real_estate_pro/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path('about/', include('about.urls')),
     path('services/', include('services.urls')),
+    path('contact/', include('contact.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
