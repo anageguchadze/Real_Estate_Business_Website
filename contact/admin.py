d161c9d6ccf3d861c650439ecc05c1212ec382a0
Ana <anageguchadze22@gmail.com>
2025-04-11 15:46:42 +0400

    Add admin for OfficeType, InquiryType and HeardFrom.

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import ContactMessage, OfficeLocation
+from .models import InquiryType, HeardFrom, OfficeType, ContactMessage, OfficeLocation
 
+
+admin.site.register(InquiryType)
+admin.site.register(HeardFrom)
+admin.site.register(OfficeType)
 admin.site.register(ContactMessage)
 admin.site.register(OfficeLocation)
